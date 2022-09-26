ca786bf7c3e60ec80100944cbf0da51998fda02d
thanigavel <thanigaisolutions@gmail.com>
2022-09-26 15:53:05 +0530

    mobile responsive

--- utility.py
+++ utility.py
@@ -6,15 +6,12 @@ from email.message import EmailMessage
 
 def getBlogData():
     # https://mocki.io/fake-json-api --> json website
-    #end_point = "https://mocki.io/v1/f4aff8ed-6d04-43e1-ac4f-ce373a4a64e0"
     return requests.get(os.getenv("BLOG_END_POINT")).json()
 
 def randomNumberGenerator():
     return random.randint(1, 10)
 
 def send_mail(name,mail,mobile,msg):
-    userName = 'thanigaisolutions@gmail.com'
-    passWord = 'xblntktjuvzkkthf'
     senderMail = 'thanigaisolutions@gmail.com'
     receipient_mail = 'thanigaifacts@gmail.com'
 
@@ -27,7 +24,7 @@ def send_mail(name,mail,mobile,msg):
     mailMsg.set_content(msgfile)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(userName, passWord)
+        smtp.login(os.getenv("MAIL_USER_NAME"), os.getenv("MAIL_PASSWORD"))
         smtp.send_message(mailMsg)
 
 
