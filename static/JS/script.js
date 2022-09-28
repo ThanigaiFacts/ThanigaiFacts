9d024a5c043a3b236d736a294f3b0d332c909028
thanigavel <thanigaisolutions@gmail.com>
2022-09-28 11:48:01 +0530

    added Java script

---
+++ static/JS/script.js
@@ -0,0 +1,38 @@
+function actionChanged(){
+        //var selectElement = event.target;
+        //var value = selectElement.value;
+        var x = document.getElementById("CompanyList")
+        var y = document.getElementById("CompName")
+        var z = document.getElementById("actionListID")
+        if (z.value == "Buy")
+         {
+          var option = document.createElement("option")
+          option.text = "Other"
+          option.value = "Other"
+          x.add(option,x[0])
+          x.selectedIndex = "0";
+          y.value = "";
+        }
+        else{
+        if (x.options[0].value == "Other")
+        {
+         x.remove(0);
+        }
+        companyListChanged();
+        }
+
+}
+function companyListChanged(){
+  var x = document.getElementById("CompanyList")
+  var y = document.getElementById("CompName")
+  if(x[x.selectedIndex].value == "Other")
+  {
+   y.value = "";
+  }
+  else{
+     y.value = x[x.selectedIndex].value;
+  }
+
+}
+actionChanged();
+
