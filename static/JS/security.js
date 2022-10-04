c8dd32f57983300d600e6f8cbdeb8e7fcf8dec2f
thanigavel <thanigaisolutions@gmail.com>
2022-10-04 10:52:59 +0530

    added alert pop up

--- static/JS/security.js
+++ static/JS/security.js
@@ -16,13 +16,13 @@ window.onunload = function () { null };
 
 
 $(document).bind("contextmenu",function(e){
-  alert("Right Click is Prevented");
+  show_alert("Right Click is Prevented");
   return false;
 });
 
 $('body').bind('cut copy paste',function(e) {
     e.preventDefault();
-    alert("You are not allowed to use Cut, Copy, Paste operations");
+    show_alert("You are not allowed to use Cut, Copy, Paste operations");
     return false;
 });
 
